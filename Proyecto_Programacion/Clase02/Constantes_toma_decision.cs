bb93a538294c15229cc176f4300c03e253fc8e2d
Carlos Azcarraga Esquivel <tasajos@gmail.com>
2026-08-31 16:38:31 -0400

    toma_decision
    update

---
+++ Proyecto_Programacion/Clase02/Constantes_toma_decision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Programacion.Clase02
+{
+    public class Constantes_toma_decision
+    {
+
+        public static void Main (string[] args)
+
+        {
+
+            int opcion = 1; //constante
+            int declaracion; //variable
+
+            Console.WriteLine("======================");
+            Console.WriteLine("======================");
+            Console.WriteLine("======================");
+            Console.WriteLine("======================");
+            Console.WriteLine("Bienvenido a la calculadora");
+            Console.WriteLine("Por favor ingrese una opcion");
+            Console.WriteLine("1.- Suma");
+            Console.WriteLine("2.- Resta");
+            Console.WriteLine("3.- Multiplicacion");
+            Console.WriteLine("4.- Division");
+            declaracion = int.Parse(Console.ReadLine());
+
+            if (declaracion ==opcion)
+                
+            {
+
+                Console.WriteLine("Es una suma " + opcion);
+
+            } else {
+                Console.WriteLine("No es uno");
+
+            }
+
+        }
+
+    }
+
+}
--- Proyecto_Programacion/Program.cs
+++ Proyecto_Programacion/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hola ssss");
+
         }
+
     }
 }
