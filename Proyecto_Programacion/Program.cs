6f2d7f14e76c10138f035b32bf09b4a0b483f27d
Carlos Azcarraga Esquivel <tasajos@gmail.com>
2026-08-31 16:10:11 -0400

    init 0
    update

---
+++ Proyecto_Programacion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_Programacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
