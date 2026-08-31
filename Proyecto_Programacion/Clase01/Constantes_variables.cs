607ea2f60df217ee0a4a3d7a19dc656ed2aa355b
Carlos Azcarraga Esquivel <tasajos@gmail.com>
2026-08-31 16:24:21 -0400

    init_2
    ccc

---
+++ Proyecto_Programacion/Clase01/Constantes_variables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Programacion.Clase01
+{
+    public class Constantes_variables
+    {
+                    public static  void Main (string[] args)
+                    {
+
+                    int edad;
+                    string nombre;
+
+                    Console.WriteLine("Buenos dias por favor ingrese su edad ");
+                    edad = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Buenos dias por favor deme su nombre ");
+                    nombre = Console.ReadLine();
+
+                    Console.WriteLine("=============================");
+                    Console.WriteLine("=============================");
+                    Console.WriteLine("=============================");
+                    Console.WriteLine("Gracias " + nombre + " " + " Tu edad es " + edad);
+
+
+
+        }
+    }
+}
